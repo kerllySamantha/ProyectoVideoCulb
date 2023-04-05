3d99d97e7d508a483d810ce4d9e775e6488e49e1
aineanton02 <ainantest@alu.edu.gva.es>
2023-04-05 19:02:54 +0200

    Modificar el Main Crear una nueva ventana para dar de alta a un Socio Crear un metodo para menuBar He subido java del videojuego

---
+++ src/videoclub/FormVideoJuego.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class FormVideoJuego {
+    private JPanel panelPrincipalVideoJuego;
+    private JLabel lblTitulo;
+    private JLabel lblAutor;
+    private JLabel lblFormato;
+    private JLabel lblAnio;
+    private JLabel lblPlataforma;
+    private JRadioButton CDRadioButton;
+    private JRadioButton DVDRadioButton;
+    private JRadioButton BLURAYRadioButton;
+    private JRadioButton ARCHIVORadioButton;
+    private JTextField textTitulo;
+    private JTextField textAutor;
+    private JTextField textAnio;
+    private JTextField textPlataforma;
+    private JButton btnCrear;
+}
