989f29f63e486bd552d193d13279d909cbdb3e3b
kerlly <kercharam@alu.edu.gva.es>
2023-04-27 12:38:58 +0200

    Gestionar errores Socio

--- src/videoclub/view/FormAltaDisco.java
+++ src/videoclub/view/FormAltaDisco.java
@@ -8,4 +8,6 @@ public class FormAltaDisco extends JFrame{
     private JPanel panelAltaDisco;
     private JLabel lblTitulo;
     private JLabel lblAutor;
+
+
 }
--- src/videoclub/view/FormAltaSocio.java
+++ src/videoclub/view/FormAltaSocio.java
@@ -4,8 +4,6 @@ import controller.GestionSocioVideoClub;
 import model.Socio;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 
@@ -23,15 +21,18 @@ public class FormAltaSocio extends JFrame {
     private JLabel poblacionSocioAlta;
     private JTextArea txtDatosSocio;
     private JButton btnRestValues;
+    private JComboBox<String> cmbProvincias;
 
     private String nifSocio, nombreSocio, poblacionSocio;
     private boolean existeNif;
     Socio socio;
 
     public FormAltaSocio() {
+
         super.setContentPane(altasSocioMenu);
         super.setDefaultCloseOperation(EXIT_ON_CLOSE);
         super.setJMenuBar(MenuBar.crearMenuBar());
+        MenuBar.gestionDeVentanas();
         btnCrearSocio.addActionListener(actionEvent -> {
             //do {
             nifSocio = txtNIFSocioAlta.getText().toUpperCase();
@@ -41,15 +42,21 @@ public class FormAltaSocio extends JFrame {
             }
             // } while (existeNif);
             nombreSocio = txtNombreSocioAlta.getText().toUpperCase();
-            poblacionSocio = txtPoblacionSocioAlta.getText().toUpperCase();
-
+            //poblacionSocio = txtPoblacionSocioAlta.getText().toUpperCase();
+            poblacionSocio = cmbProvincias.getSelectedItem().toString();
             this.socio = new Socio(nifSocio, nombreSocio, "", poblacionSocio);
+            if(nombreSocio.equalsIgnoreCase("") || nifSocio.equalsIgnoreCase("")){
+                JOptionPane.showMessageDialog(null, "No se pude crear un Socio sin NIF o nombre");
+                socios.remove(this.socio);
+            }
+            else {
+                socios.add(this.socio);
+                txtDatosSocio.setText(socio.toString());
+            }
 
-            txtDatosSocio.setText(socio.toString());
             txtNIFSocioAlta.setText("");
             txtNombreSocioAlta.setText("");
 
-            socios.add(socio);
 
 
         });
@@ -57,8 +64,13 @@ public class FormAltaSocio extends JFrame {
             txtNombreSocioAlta.setText("");
             txtNIFSocioAlta.setText("");
             txtNombreSocioAlta.setText("");
-            txtPoblacionSocioAlta.setText("");
+            //txtPoblacionSocioAlta.setText("");
+            cmbProvincias.setSelectedIndex(0);
+            txtDatosSocio.setText("");
+
+
         });
     }
+
 
 }
