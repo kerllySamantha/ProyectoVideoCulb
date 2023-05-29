fb61cf788cd32a79fe155cad02443295dd450d44
aineanton02 <ainantest@alu.edu.gva.es>
2023-05-29 15:51:03 +0200

    Terminado el form de devoluciones

--- src/videoclub/view/FormDevoluciones.java
+++ src/videoclub/view/FormDevoluciones.java
@@ -29,17 +29,24 @@ public class FormDevoluciones extends JFrame{
         super.setJMenuBar(MenuBar.crearMenuBar());
         MenuBar.gestionDeVentanas();
 
-
         btnComprobarSocio.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                socioNIF = txtFieldNifSocio.getText().toUpperCase();
-                existeNif = GestionSocioVideoClub.comprobarNif(GestionSocioVideoClub.socios,socioNIF);
-                if(existeNif) {
-                    JOptionPane.showMessageDialog(null,"No existe el NIF introducido");
-                } else {
-                    //listaDevolver.setListData(lista.devolverMult(multimedia,socios));
-                    listaDevolver.setModel(GestionSocioVideoClub.mostarMultSocio(txtFieldNifSocio.getText()));
+                try {
+                    socioNIF = txtFieldNifSocio.getText().toUpperCase();
+                    existeNif = GestionSocioVideoClub.comprobarNif(GestionSocioVideoClub.socios,socioNIF);
+
+                    if (socioNIF.equalsIgnoreCase("")) {
+                        JOptionPane.showMessageDialog(null,"El campo NIF Socio está vacio");
+
+                    } else if (!existeNif) {
+                        JOptionPane.showMessageDialog(null,"No existe el NIF introducido");
+
+                    } else {
+                        listaDevolver.setModel(GestionSocioVideoClub.mostarMultSocio(txtFieldNifSocio.getText()));
+                    }
+                }catch (Exception e1){
+                    e1.printStackTrace();
                 }
             }
         });
@@ -50,7 +57,6 @@ public class FormDevoluciones extends JFrame{
                 int respuesta = JOptionPane.showConfirmDialog(null,"Estás seguro que quieres devolver-lo?","Alerta!",JOptionPane.YES_NO_OPTION);
                 if (respuesta == JOptionPane.YES_OPTION) {
                     GestionSocioVideoClub.devolverMultimedia(txtFieldNifSocio.getText(), listaDevolver.getSelectedValue().toString());
-                    //GestionLogs.escribirRegistro(GestionLogs.registroDevolucionMuolt(txtFieldNifSocio.getText(), listaDevolver.getSelectedValue().toString()));
                 }
             }
         });
