283c65932e76ee78c5774a226ed4dcd5c9f76762
aineanton02 <ainantest@alu.edu.gva.es>
2023-05-30 20:36:53 +0200

    Cambios en la clase videojuego

--- src/videoclub/view/FormVideoJuego.java
+++ src/videoclub/view/FormVideoJuego.java
@@ -1,14 +1,12 @@
 package view;
 
 import model.Formato;
-import model.Multimedia;
 import model.Plataforma;
 import model.Videojuego;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Objects;
 
 public class FormVideoJuego extends JFrame {
 
@@ -34,109 +32,68 @@ public class FormVideoJuego extends JFrame {
     private JRadioButton nintendoRadioButton;
     private JRadioButton PCRadioButton;
     private JTextArea visualizarDatos;
-    private String tituloVideoJuego,autorVideoJuego;
-    private int anioVideoJuego;
-    private boolean validar;
-    Videojuego juego, plataformaVideoJuego;
-
+    Videojuego juego;
 
     public FormVideoJuego() {
         super.setContentPane(panelPrincipalVideoJuego);
         super.setLocationRelativeTo(null);
         super.setJMenuBar(MenuBar.crearMenuBar());
         MenuBar.gestionDeVentanas();
+        altaVideoJuego();
+    }
+    public void altaVideoJuego(){
         btnCrear.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try{
-                    tituloVideoJuego = textTitulo.getText().toUpperCase();
-                    autorVideoJuego = textAutor.getText().toUpperCase();
-                    anioVideoJuego = Integer.parseInt(Objects.requireNonNull(comboBox1.getSelectedItem()).toString());
+                try {
+                    String tituloVideoJuego = textTitulo.getText().toUpperCase();
+                    String autorVideoJuego = textAutor.getText().toUpperCase();
+                    int anioVideoJuego = 0;
+                    Formato formato = null;
+                    Plataforma plataforma = null;
+                    boolean datos = true;
 
-                    if(validar){
-                        visualizarDatos.setText(" ");
-                        JOptionPane.showMessageDialog(null,"Prueba");
-                    } else if ((tituloVideoJuego.equalsIgnoreCase("")) && (autorVideoJuego.equalsIgnoreCase(""))) {
-                        JOptionPane.showMessageDialog(null,"El campo titulo y el campo autor están vacíos");
-                        visualizarDatos.setText("");
-                    } else if (tituloVideoJuego.equalsIgnoreCase("")){
-                        JOptionPane.showMessageDialog(null,"El campo título está vacío");
-                        visualizarDatos.setText("");
-                    } else if (autorVideoJuego.equalsIgnoreCase("")) {
-                        JOptionPane.showMessageDialog(null,"El campo autor está vacío");
-                        visualizarDatos.setText("");
-
+                    if (textTitulo.getText().equals("") && textAnio.getText().equals("")){
+                        datos = false;
+                        JOptionPane.showMessageDialog(null,"Los campos Titulo y Autor están vacíos.");
+                    } else if (textTitulo.getText().equals("")) {
+                        datos = false;
+                        JOptionPane.showMessageDialog(null,"El campo título está vacío.");
+                    } else if (textAutor.getText().equals("")) {
+                        datos = false;
+                        JOptionPane.showMessageDialog(null,"El campo autor está vacío.");
                     } else {
-                        juego = new Videojuego(tituloVideoJuego,autorVideoJuego,Formato.BLUERAY,anioVideoJuego,Plataforma.PC);
-                        visualizarDatos.setText(juego.toString());
+                        anioVideoJuego=Integer.parseInt(comboBox1.getSelectedItem().toString());
                     }
 
-                    textTitulo.setText(" ");
-                    textAutor.setText(" ");
-                } catch (Exception e2){
-                    e2.printStackTrace();
+                    if (CDRadioButton.isSelected()) {
+                        formato = Formato.CD;
+                    } else if (DVDRadioButton.isSelected()) {
+                        formato = Formato.DVD;
+                    } else if (BLURAYRadioButton.isSelected()) {
+                        formato = Formato.BLUERAY;
+                    } else if (ARCHIVORadioButton.isSelected()) {
+                        formato = Formato.ARCHIVO;
+                    }
+
+                    if (playRadioButton.isSelected()) {
+                        plataforma = Plataforma.PlayStation5;
+                    } else if (XBoxRadioButton.isSelected()) {
+                        plataforma = Plataforma.XBox;
+                    } else if (nintendoRadioButton.isSelected()) {
+                        plataforma = Plataforma.NintendoSwitch;
+                    } else if (PCRadioButton.isSelected()) {
+                        plataforma = Plataforma.PC;
+                    }
+
+                    if (datos) {
+                        juego = new Videojuego(tituloVideoJuego,autorVideoJuego,formato,anioVideoJuego,plataforma);
+                        visualizarDatos.setText(juego.toString());
+                    }
+                } catch (Exception e1) {
+                    e1.printStackTrace();
                 }
             }
         });
-    }
-    public void altaVideoJuego(){
-        try {
-            String titulo = "";
-            String autor = "";
-            int anio = 0;
-            Formato formato = null;
-            Plataforma plataforma = null;
-            boolean datos = true;
-
-            if (!textTitulo.getText().equals("")) {
-                titulo = textTitulo.getText().toUpperCase();
-            } else {
-                datos = false;
-            }
-            if (!textAutor.getText().equals("")) {
-                autor = textAutor.getText().toUpperCase();
-            } else {
-                datos = false;
-            }
-            if (!Objects.equals(comboBox1.getSelectedItem(), 0)) {
-                datos = false;
-            } else {
-                anio = Integer.parseInt(String.valueOf(comboBox1.getSelectedIndex()));
-            }
-            if (!textPlataforma.getText().equals("")) {
-                plataforma = Plataforma.valueOf(textPlataforma.getText().toUpperCase());
-            } else {
-                datos = false;
-            }
-
-
-            if (CDRadioButton.isSelected()) {
-                formato = Formato.CD;
-
-            } else if (DVDRadioButton.isSelected()) {
-                formato = Formato.DVD;
-                datos = false;
-            } else if (BLURAYRadioButton.isSelected()) {
-                formato = Formato.BLUERAY;
-                datos = false;
-            } else if (ARCHIVORadioButton.isSelected()) {
-                formato = Formato.ARCHIVO;
-                datos = false;
-            }
-
-            if(playRadioButton.isSelected()){
-                plataforma = Plataforma.PlayStation5;
-            } else if (XBoxRadioButton.isSelected()) {
-                plataforma = Plataforma.XBox;
-            } else if (nintendoRadioButton.isSelected()) {
-                plataforma = Plataforma.NintendoSwitch;
-            } else if (PCRadioButton.isSelected()) {
-                plataforma=Plataforma.PC;
-            }
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        
     }
 }
