612b983f6fa42551d5040901f60a4594c0937890
kerlly <kercharam@alu.edu.gva.es>
2023-06-03 12:36:34 +0200

    gestionar cosas hehe

--- src/videoclub/view/FormPrincipalVideoClub.java
+++ src/videoclub/view/FormPrincipalVideoClub.java
@@ -26,21 +26,19 @@ public class FormPrincipalVideoClub extends JFrame {
         Color fondoBotones = new Color(214,220,255);
 
         super.setResizable(false);
-        super.setLayout(null);
+        super.setBounds((MenuBar.ANCHO_PANTALLA/2)-200, (MenuBar.ALTO_PANTALLA/2)-150, 400,400);
+        super.setLocationRelativeTo(null);
         super.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        super.setBounds(100, 100, 250, 500);
 
         panelPrincipal = new JPanel();
         panelPrincipal.setBackground(fondo);
         super.setContentPane(panelPrincipal);
-        //add(panelPrincipal, BorderLayout.CENTER);
-        super.setLocationRelativeTo(null);
-        panelPrincipal.setLayout(new FlowLayout());
+        panelPrincipal.setBounds((MenuBar.ANCHO_PANTALLA/2)-250, (MenuBar.ALTO_PANTALLA/2)-125, 250,500);
+        panelPrincipal.setLayout(null);
 
         botonAltas = new JButton("Altas");
-        botonAltas.setMinimumSize(new Dimension(20, 20));
-        botonAltas.setMaximumSize(new Dimension(250, 60));
-        botonAltas.setPreferredSize(new Dimension(200, 30));
+        botonAltas.setBounds((super.getWidth()/2)-125, 20, 250,50);
+        botonAltas.setFont(new Font("Arial", Font.BOLD, 20));
         botonAltas.setBackground(fondoBotones);
         botonAltas.setForeground(fondo);
         botonAltas.setFont(new Font("Arial",Font.BOLD,18));
@@ -64,36 +62,32 @@ public class FormPrincipalVideoClub extends JFrame {
         //btnBajas.setFont(new Font("Titulo", Font.BOLD, 30));
 
         btnAlquiler = new JButton("Alquiler");
-        btnAlquiler.setMinimumSize(new Dimension(20, 20));
-        btnAlquiler.setMaximumSize(new Dimension(250, 60));
-        btnAlquiler.setPreferredSize(new Dimension(200, 30));
+        btnAlquiler .setFont(new Font("Arial", Font.BOLD, 20));
+        btnAlquiler .setBounds((super.getWidth()/2)-125, 80, 250,50);
         btnAlquiler.setBackground(fondoBotones);
         btnAlquiler.setForeground(fondo);
         btnAlquiler.setFont(new Font("Arial",Font.BOLD,16));
         panelPrincipal.add(btnAlquiler);
 
         btnDevolucion = new JButton("Devolución");
-        btnDevolucion.setMinimumSize(new Dimension(20, 20));
-        btnDevolucion.setMaximumSize(new Dimension(250, 60));
-        btnDevolucion.setPreferredSize(new Dimension(200, 30));
+        btnDevolucion.setFont(new Font("Arial", Font.BOLD, 20));
+        btnDevolucion.setBounds((super.getWidth()/2)-125, 140, 250,50);
         btnDevolucion.setBackground(fondoBotones);
         btnDevolucion.setForeground(fondo);
         btnDevolucion.setFont(new Font("Arial",Font.BOLD,16));
         panelPrincipal.add(btnDevolucion);
 
         btnListados = new JButton("Listar");
-        btnListados.setMinimumSize(new Dimension(20, 20));
-        btnListados.setMaximumSize(new Dimension(250, 60));
-        btnListados.setPreferredSize(new Dimension(200, 30));
+        btnListados.setFont(new Font("Arial", Font.BOLD, 20));
+        btnListados.setBounds((super.getWidth()/2)-125, 200, 250,50);
         btnListados.setBackground(fondoBotones);
         btnListados.setForeground(fondo);
         btnListados.setFont(new Font("Arial",Font.BOLD,16));
         panelPrincipal.add(btnListados);
 
         btnPagos = new JButton("Pagos");
-        btnPagos.setMinimumSize(new Dimension(20, 20));
-        btnPagos.setMaximumSize(new Dimension(250, 60));
-        btnPagos.setPreferredSize(new Dimension(200, 30));
+        btnPagos.setFont(new Font("Arial", Font.BOLD, 20));
+        btnPagos.setBounds((super.getWidth()/2)-125, 260, 250,50);
         btnPagos.setBackground(fondoBotones);
         btnPagos.setForeground(fondo);
         btnPagos.setFont(new Font("Arial",Font.BOLD,16));
@@ -102,7 +96,7 @@ public class FormPrincipalVideoClub extends JFrame {
 
         botonAltas.addActionListener(e -> {
             MenuBar.formPrincipalAltas.setDefaultCloseOperation(MenuBar.formPrincipalAltas.HIDE_ON_CLOSE);
-            MenuBar.formPrincipalAltas.setBounds(100, 100, 600, 300);
+            MenuBar.formPrincipalAltas.setBounds((MenuBar.ANCHO_PANTALLA/2)-200, (MenuBar.ALTO_PANTALLA/2)-225, 400,400);
             if (!MenuBar.formPrincipalAltas.isVisible()) {
                 MenuBar.formPrincipalAltas.setVisible(true);
             }
@@ -110,15 +104,15 @@ public class FormPrincipalVideoClub extends JFrame {
 
         btnAlquiler.addActionListener(e -> {
             MenuBar.formAlquiler.setDefaultCloseOperation(MenuBar.formAlquiler.HIDE_ON_CLOSE);
-            MenuBar.formAlquiler.setBounds(100,100,600,300);
-            if(!MenuBar.formAlquiler.isVisible()) {
+            MenuBar.formAlquiler.setBounds((MenuBar.ANCHO_PANTALLA/2)-250, (MenuBar.ALTO_PANTALLA/2)-275, 500,500);
+            if (!MenuBar.formAlquiler.isVisible()) {
                 MenuBar.formAlquiler.setVisible(true);
             }
         });
 
         btnDevolucion.addActionListener(e -> {
             MenuBar.formDevoluciones.setDefaultCloseOperation(MenuBar.formDevoluciones.HIDE_ON_CLOSE);
-            MenuBar.formDevoluciones.setBounds(100,100,600,300);
+            MenuBar.formDevoluciones.setBounds((MenuBar.ANCHO_PANTALLA/2)-250, (MenuBar.ALTO_PANTALLA/2)-275, 500,500);
             if (!MenuBar.formDevoluciones.isVisible()) {
                 MenuBar.formDevoluciones.setVisible(true);
             }
@@ -126,7 +120,7 @@ public class FormPrincipalVideoClub extends JFrame {
 
         btnListados.addActionListener(e -> {
             MenuBar.formListadosMultimedia.setDefaultCloseOperation(MenuBar.formListadosMultimedia.HIDE_ON_CLOSE);
-            MenuBar.formListadosMultimedia.setBounds(100,100,600,300);
+            MenuBar.formListadosMultimedia.setBounds((MenuBar.ANCHO_PANTALLA/2)-250, (MenuBar.ALTO_PANTALLA/2)-275, 500,500);
             if (!MenuBar.formListadosMultimedia.isVisible()) {
                 MenuBar.formListadosMultimedia.setVisible(true);
             }
@@ -134,7 +128,7 @@ public class FormPrincipalVideoClub extends JFrame {
 
         btnPagos.addActionListener(e -> {
             MenuBar.formPagoRecargo.setDefaultCloseOperation(MenuBar.formPagoRecargo.HIDE_ON_CLOSE);
-            MenuBar.formPagoRecargo.setBounds(100,100,600,300);
+            MenuBar.formPagoRecargo.setBounds((MenuBar.ANCHO_PANTALLA/2)-250, (MenuBar.ALTO_PANTALLA/2)-275, 500,500);
             if (!MenuBar.formPagoRecargo.isVisible()) {
                 MenuBar.formPagoRecargo.setVisible(true);
             }
